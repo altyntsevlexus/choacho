319064a8fdc3c232b68d041d4962bfbc4ac3e61d
Oleksii Altyntsev <oleksii.a@propertymate.ai>
2023-05-08 18:52:27 +0300

    fix

--- src/components/sections/Reviews/Reviews.tsx
+++ src/components/sections/Reviews/Reviews.tsx
@@ -29,6 +29,7 @@ const Reviews = () => (
       <iframe
         src="https://www.instagram.com/cho_a_cho/embed"
         title="instagram"
+        loading="lazy"
         height={456}
         width={440}
         className={styles.instagram}
